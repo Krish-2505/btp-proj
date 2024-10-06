9f4f8b003934f3be19447121d97f172cfd48437d
krish <mukku21bcs154@iiitkottayam.ac.in>
2024-10-06 16:23:47 +0530

    modifying resnet model

--- fakeimagedet/networks/resnet_mod.py
+++ fakeimagedet/networks/resnet_mod.py
@@ -54,25 +54,13 @@ class ComplexFCBlock(nn.Module):
         self.bn1 = nn.BatchNorm1d(1024)
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(p=0.4)
-        
-        self.fc2 = nn.Linear(1024, 512)
-        self.bn2 = nn.BatchNorm1d(512)
-        
-        self.fc3 = nn.Linear(512, 128)
-        self.fc4 = nn.Linear(128, 1)
+        self.fc4 = nn.Linear(1024, 1)
     
     def forward(self, x):
         x = self.fc1(x)
         x = self.bn1(x)
         x = self.relu(x)
         x = self.dropout(x)
-        
-        x = self.fc2(x)
-        x = self.bn2(x)
-        x = self.relu(x)
-        
-        x = self.fc3(x)
-        x = self.relu(x)
         x = self.fc4(x)
         
         return x
